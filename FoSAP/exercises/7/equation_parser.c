a5148205b58b9ec6cac3ea541802c06b4e27c600
Dragonsight91 <r.vanemelen@icloud.com>
2020-05-29 21:50:11 +0200

    correct some memory management issues

--- FoSAP/exercises/7/equation_parser.c
+++ FoSAP/exercises/7/equation_parser.c
@@ -9,15 +9,15 @@
 
 //  Parser Symbol definitions
 
-// basic operators 
-#define NOT  '~'
-#define OR   '|'
-#define AND  '&'
+// basic operators
+#define NOT '~'
+#define OR '|'
+#define AND '&'
 
 // compound operators (TODO: IMPLEMENT)
-#define XOR  '%'
-#define IMP  '@'
-#define EQI  '='
+#define XOR '%'
+#define IMP '@'
+#define EQI '='
 
 //struct definitions
 struct node
@@ -43,12 +43,16 @@ struct node *newNode()
 void parser(struct node *curr, char *eq, int len)
 {
     printf("\nlength: %d\n", len);
+    if (len == 0)
+        return;
+
     if (len == 1)
     {
         curr->data = *eq;
         printf("Symbol: %c\n", *eq);
         return;
     }
+
     // keep track of our position
     int opIdx = -1; // the operand index. this is the location of any given operand in the statement
     int brck = 0;   // the currently opened brackets. if it's not 0, we can't look further, because the algorithm can't parse it.
@@ -77,48 +81,39 @@ void parser(struct node *curr, char *eq, int len)
     if (opIdx != -1)
     {
         // set te length of the partitions
-        int lenA = opIdx;
-        int lenB = len - opIdx-1;
+        int lenA = opIdx+1;
+        int lenB = len - opIdx;
 
         // create the partitions
-        char* partitionA = malloc(lenA+1); // partition a goes up to the operator's index
-        char* partitionB = malloc(lenB+1); // partition b starts after the operator's index
+        char *partitionA = malloc(lenA); // partition a goes up to the operator's index
+        char *partitionB = malloc(lenB); // partition b starts after the operator's index
 
-        *(partitionA+lenA+1) = '\0';
-        *(partitionB+lenB+1) = '\0';
+        *(partitionA + lenA) = '\0';
+        *(partitionB + lenB) = '\0';
 
         // copy the data over. this adds space complexity, but keeps it easier to read
-        strncpy(partitionA, eq, opIdx);
-        strncpy(partitionB, eq + opIdx + 1, len-1);
-        
+        strncpy(partitionA, eq, lenA-1);
+        strncpy(partitionB, eq+opIdx+1, lenB-1);
 
         // print the partitions to stdout. DEBUG ONLY!!!
-        printf("Expression: %s\n", eq);
-        printf("partition A: %s\n", partitionA);
-        printf("partition B: %s\n", partitionB);
+        if (DEBUG)
+        {
+            printf("Expression: %s\n", eq);
+            printf("partition A: %d\n", lenA);
+            printf("partition B: %d\n", lenB);
+        }
 
         // assign the operator
         struct node *a = newNode();
         struct node *b = newNode();
+    
+        parser(a, partitionA, lenA-1);
+        free(partitionA);
 
-        // fork the process, so we can use async computation
-        pid_t PID = fork();
-        if (PID == 0)
-        {
-            // create a new node
+        parser(b, partitionB, lenB-1);
+        free(partitionB);
 
-            parser(a, partitionA, lenA);
-            exit(0); // clean exit the child
-        }
-        else
-        {
-            parser(b, partitionB, lenB);
-        }
-
-        // wait for all children to finish, no zombie children allowed
-        wait(NULL);
-
-        curr->data = *(eq+opIdx);
+        curr->data = *(eq + opIdx);
         curr->left = a;
         curr->right = b;
 
@@ -128,37 +123,43 @@ void parser(struct node *curr, char *eq, int len)
     {
         printf("Expression: %s\n", eq);
         printf("no operator\n");
+        printf("BOOL: %c\n", *(eq + len-1));
 
-        // somehow it doesn't work.... WHY?
-        if ((*eq == '(') & (*(eq+len-1) == ')'))
+        // (~1)\0
+        if ((*eq == '(') & (*(eq + len-1) == ')'))
         {
             printf("this is fenced\n");
             // remove fencing
-            char temp[strlen(eq)-1];
-            *(temp+(strlen(eq)-1)) = '\0';
+            char* temp = malloc(len-1);
+            *(temp + len-1) = '\0';
             strncpy(temp, eq+1, len-2);
 
             printf("unfenced eq: %s\n", temp);
             // try again
-            parser(curr, temp, strlen(temp));
-        }else if (*(eq) == '~')
+            parser(curr, temp, len-2);
+            free(temp);
+            return;
+        }
+        else if (*(eq) == '~')
         {
             printf("negated");
-            struct node* leaf = newNode();
-            int newlen = len-1;
-            char* newstr = malloc(newlen);
+            struct node *leaf = newNode();
+            int newlen = len - 1;
+            char *newstr = malloc(newlen);
 
-            strncpy(newstr, eq+1, newlen);
+            strncpy(newstr, eq + 1, newlen);
 
             curr->data = '~';
             curr->left = leaf;
             parser(leaf, newstr, newlen);
-            
-        }else{
-            printf("Error while parsing.");
+            free(newstr);
+            return;
         }
-        
-        
+        else
+        {
+            printf("Error while parsing.");
+            return;
+        }
     }
 }
 
@@ -173,13 +174,15 @@ void eqBuilder(char **out, struct node **curr)
 
 int main(void)
 {
+    FILE* fh = fopen("boolsche-ausdruecke", "r");
     struct node *root = newNode();
-    char *test = "~1|(1|0&(0))&1";
+    char *test = "(~1&~~0|1|0&~0&0|0&1&1|~0&(0))&((~(~0)|0&0|1&0|1))|0&1&(~((~(1|~((1))&1&(0))))|(1))|~0&(~0|0|~0|1&1)&(~0&1|0)&0|1&0|1&0&(~1|~1&~1&0)|~0|0|1&0|0|(~(1)&((0|1|~~0|0|(1)&(1&(0)))))&(~1&~~0|1|0&~0&0|0&1&1|~0&(0))&((~(~0)|0&0|1&0|1))|0&1&(~((~(1|~((1))&1&(0))))|(1))|~0&(~0|0|~0|1&1)&(~0&1|0)&0|1&0|1&0&(~1|~1&~1&0)|~0|0|1&0|0|(~(1)&((0|1|~~0|0|(1)&(1&(0)))))&(~1&~~0|1|0&~0&0|0&1&1|~0&(0))&((~(~0)|0&0|1&0|1))|0&1&(~((~(1|~((1))&1&(0))))|(1))|~0&(~0|0|~0|1&1)&(~0&1|0)&0|1&0|1&0&(~1|~1&~1&0)|~0|0|1&0|0|(~(1)&((0|1|~~0|0|(1)&(1&(0)))))&(~1&~~0|1|0&~0&0|0&1&1|~0&(0))&((~(~0)|0&0|1&0|1))|0&1&(~((~(1|~((1))&1&(0))))|(1))|~0&(~0|0|~0|1&1)&(~0&1|0)&0|1&0|1&0&(~1|~1&~1&0)|~0|0|1&0|0|(~(1)&((0|1|~~0|0|(1)&(1&(0)))))&(~1&~~0|1|0&~0&0|0&1&1|~0&(0))&((~(~0)|0&0|1&0|1))|0&1&(~((~(1|~((1))&1&(0))))|(1))|~0&(~0|0|~0|1&1)&(~0&1|0)&0|1&0|1&0&(~1|~1&~1&0)|~0|0|1&0|0|(~(1)&((0|1|~~0|0|(1)&(1&(0)))))&(~1&~~0|1|0&~0&0|0&1&1|~0&(0))&((~(~0)|0&0|1&0|1))|0&1&(~((~(1|~((1))&1&(0))))|(1))|~0&(~0|0|~0|1&1)&(~0&1|0)&0|1&0|1&0&(~1|~1&~1&0)|~0|0|1&0|0|(~(1)&((0|1|~~0|0|(1)&(1&(0)))))&(~1&~~0|1|0&~0&0|0&1&1|~0&(0))&((~(~0)|0&0|1&0|1))|0&1&(~((~(1|~((1))&1&(0))))|(1))|~0&(~0|0|~0|1&1)&(~0&1|0)&0|1&0|1&0&(~1|~1&~1&0)|~0|0|1&0|0|(~(1)&((0|1|~~0|0|(1)&(1&(0)))))&(~1&~~0|1|0&~0&0|0&1&1|~0&(0))&((~(~0)|0&0|1&0|1))|0&1&(~((~(1|~((1))&1&(0))))|(1))|~0&(~0|0|~0|1&1)&(~0&1|0)&0|1&0|1&0&(~1|~1&~1&0)|~0|0|1&0|0|(~(1)&((0|1|~~0|0|(1)&(1&(0)))))&(~1&~~0|1|0&~0&0|0&1&1|~0&(0))&((~(~0)|0&0|1&0|1))|0&1&(~((~(1|~((1))&1&(0))))|(1))|~0&(~0|0|~0|1&1)&(~0&1|0)&0|1&0|1&0&(~1|~1&~1&0)|~0|0|1&0|0|(~(1)&((0|1|~~0|0|(1)&(1&(0)))))&(~1&~~0|1|0&~0&0|0&1&1|~0&(0))&((~(~0)|0&0|1&0|1))|0&1&(~((~(1|~((1))&1&(0))))|(1))|~0&(~0|0|~0|1&1)&(~0&1|0)&0|1&0|1&0&(~1|~1&~1&0)|~0|0|1&0|0|(~(1)&((0|1|~~0|0|(1)&(1&(0)))))&(~1&~~0|1|0&~0&0|0&1&1|~0&(0))&((~(~0)|0&0|1&0|1))|0&1&(~((~(1|~((1))&1&(0))))|(1))|~0&(~0|0|~0|1&1)&(~0&1|0)&0|1&0|1&0&(~1|~1&~1&0)|~0|0|1&0|0|(~(1)&((0|1|~~0|0|(1)&(1&(0)))))&(~1&~~0|1|0&~0&0|0&1&1|~0&(0))&((~(~0)|0&0|1&0|1))|0&1&(~((~(1|~((1))&1&(0))))|(1))|~0&(~0|0|~0|1&1)&(~0&1|0)&0|1&0|1&0&(~1|~1&~1&0)|~0|0|1&0|0|(~(1)&((0|1|~~0|0|(1)&(1&(0)))))";
     int len = strlen(test);
+    
 
     printf("Expression: %s\n\n", test);
 
     parser(root, test, len);
-
+    printf("\nWord Length: %d\n", len);
     return 0;
 }
